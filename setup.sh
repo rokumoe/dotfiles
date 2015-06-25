ecd9efc67b524ab087aed266749ca4511a30e831
vizee <vizee@live.cn>
2015-06-25 10:18:49 +0800

    更新setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-ln -sf ~/.vim/_vimrc ~/.vimrc
-ln -sf ~/.vim/git/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
+cd `dirname $0`
+curdir=`pwd`
+git submodule init
+git submodule update
+ln -sf $curdir/_vimrc ~/.vimrc
+mkdir -p ~/.vim/colors/
+ln -sf $curdir/git/molokai/colors/molokai.vim ~/.vim/colors/molokai.vim
